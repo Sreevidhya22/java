e797f34aac28701f7f0d249f5a7bfc92b4218815
Sreevidhya22 <sreevidhyavenugopal22@gmail.com>
2025-02-27 14:15:50 +0530

    Add files via upload

---
+++ StringSearch.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class StringSearch{
+public static void main(String args[]){
+
+int n;
+String key = new String();
+boolean b = false;
+
+String s[] = new String[10];
+Scanner sc = new Scanner(System.in);
+Scanner scanner = new Scanner(System.in);
+
+System.out.println("Enter the no: of Strings you want to enter");
+n = sc.nextInt();
+
+System.out.println("Enter the Strings");
+for(int i = 0; i < n; i++){
+s[i] = scanner.nextLine();
+}
+
+System.out.println("Enter the element to search");
+key = scanner.nextLine();
+
+for(int i = 0; i < n; i++){
+if(s[i] == key){
+System.out.println("Element " + key + " found at index " + i);
+b = true;
+}
+}
+
+if(b == false){
+System.out.println("Not found");
+}
+}
+}
